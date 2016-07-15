8e48f2efc58c26073d512cd80e15002179e8d513
Tremblay <philippe.tremblay@ia.ca>
2016-07-15 16:17:12 -0400

    First 2 tests passed.  Refactoring pending....

--- ReferencesSwitcherForDotNet.Library/SolutionReferencesSwitcher.cs
+++ ReferencesSwitcherForDotNet.Library/SolutionReferencesSwitcher.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Build.Construction;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Evaluation;
 
 namespace ReferencesSwitcherForDotNet.Library
 {
@@ -7,7 +12,30 @@ namespace ReferencesSwitcherForDotNet.Library
         public void Switch(string solutionFullPath)
         {
             var solution = SolutionFile.Parse(solutionFullPath);
-            solution.ProjectsInOrder
+            foreach (var solutionProject in solution.ProjectsInOrder)
+            {
+                if (File.Exists(solutionProject.AbsolutePath))
+                {
+                    var project = new Project(solutionProject.AbsolutePath);
+                    var references = project.Items.Where(x=>x.ItemType == "Reference").ToList();
+                    foreach (var referenceItem in references)
+                    {
+                        var matchedSolutionProject = solution.ProjectsInOrder.FirstOrDefault(x => x.ProjectName == referenceItem.EvaluatedInclude);
+                        if (matchedSolutionProject != null)
+                        {
+                            var metadata = new List<KeyValuePair<string, string>>();
+                            metadata.Add(new KeyValuePair<string, string>("Project", matchedSolutionProject.ProjectGuid));
+                            metadata.Add(new KeyValuePair<string, string>("Name", matchedSolutionProject.ProjectName));
+                            var uri = new Uri(solutionProject.AbsolutePath);
+                            var relativePath = uri.MakeRelativeUri(new Uri(matchedSolutionProject.AbsolutePath));
+                            project.AddItem("ProjectReference", relativePath.ToString().Replace("/", Path.DirectorySeparatorChar.ToString()), metadata);
+                            project.RemoveItem(referenceItem);
+                            project.Save();
+                        }
+                    }
+                }
+            }
+            ProjectCollection.GlobalProjectCollection.UnloadAllProjects();
         }
     }
 }
--- ReferencesSwitcherForDotNet.Tests/SolutionReferencesSwitcherTest.cs
+++ ReferencesSwitcherForDotNet.Tests/SolutionReferencesSwitcherTest.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Build.Evaluation;
+using Microsoft.VisualBasic.FileIO;
 using NUnit.Framework;
 using ReferencesSwitcherForDotNet.Library;
 
@@ -15,6 +13,11 @@ namespace ReferencesSwitcherForDotNet.Tests
     [TestFixture]
     public class SolutionReferencesSwitcherTest
     {
+        private static string CurrentDir
+        {
+            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
+        }
+
         [SetUp]
         public void InitializeTest()
         {
@@ -24,35 +27,83 @@ namespace ReferencesSwitcherForDotNet.Tests
         [Test]
         public void Switch_Should_AddProjectReference_When_ReferenceExistsInSolutionForThisProjectOutput()
         {
-            var subject = new SolutionReferencesSwitcher();
+            using (var workingDir = new WorkingDirectory())
+            {
+                var subject = new SolutionReferencesSwitcher();
 
-            AssertProject2HasReferenceToProject1();
+                AssertProject2HasReferenceToProject1(workingDir);
 
-            subject.Switch(GetSolutionFileFullPath());
+                subject.Switch(GetSolutionFileFullPath(workingDir));
 
-            var project2 = GetProject2();
-            var projectReference = project2.Items.FirstOrDefault(x => x.ItemType == "ProjectReference");
-            projectReference.Should().NotBeNull();
-            projectReference.EvaluatedInclude.Should().Be(@"..\Project1\Project1.csproj");
+                var project2 = GetProject2(workingDir);
+                var projectReference = project2.Items.FirstOrDefault(x => x.ItemType == "ProjectReference");
+                projectReference.Should().NotBeNull();
+                projectReference.EvaluatedInclude.Should().Be(@"..\Project1\Project1.csproj");
+                var project1 = GetProject1(workingDir);
+                projectReference.GetMetadataValue("Project").Should().Be(project1.Properties.First(x => x.Name == "ProjectGuid").EvaluatedValue);
+                projectReference.GetMetadataValue("Name").Should().Be(project1.Properties.First(x => x.Name == "ProjectName").EvaluatedValue);
+            }
         }
-        
-        private void AssertProject2HasReferenceToProject1()
+
+        [Test]
+        public void Switch_Should_RemoveReferenceToProject_When_ReferenceExistsInSolutionForThisProjectOutput()
         {
-            var project2 = GetProject2();
+            using (var workingDir = new WorkingDirectory())
+            {
+                var subject = new SolutionReferencesSwitcher();
+
+                AssertProject2HasReferenceToProject1(workingDir);
+
+                subject.Switch(GetSolutionFileFullPath(workingDir));
+
+                var project2 = GetProject2(workingDir);
+                var reference = project2.Items.FirstOrDefault(x => x.ItemType == "Reference" && x.EvaluatedInclude == "Project1");
+                reference.Should().BeNull();
+            }
+        }
+
+        private static Project GetProject(string projectName, WorkingDirectory workingDir)
+        {
+            return new Project(workingDir.Path.PathCombine(projectName, "{0}.csproj".FormatWith(projectName)));
+        }
+
+        private void AssertProject2HasReferenceToProject1(WorkingDirectory workingDir)
+        {
+            var project2 = GetProject2(workingDir);
             project2.Items.Should().Contain(x => x.ItemType == "Reference" && x.EvaluatedInclude == "Project1");
             ProjectCollection.GlobalProjectCollection.UnloadProject(project2);
         }
 
-        private Project GetProject2()
+        private Project GetProject1(WorkingDirectory workingDir)
         {
-            return new Project(CurrentDir.PathCombine("FilesForTesting", "Project2", "Project2.csproj"));
+            return GetProject("Project1", workingDir);
         }
 
-        private string GetSolutionFileFullPath()
+        private Project GetProject2(WorkingDirectory workingDir)
         {
-            return CurrentDir.PathCombine("FilesForTesting", "SolutionFile.sln");
+            return GetProject("Project2", workingDir);
         }
 
-        private static string CurrentDir => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        private string GetSolutionFileFullPath(WorkingDirectory workingDir)
+        {
+            return workingDir.Path.PathCombine("SolutionFile.sln");
+        }
+
+        private class WorkingDirectory : IDisposable
+        {
+            public WorkingDirectory()
+            {
+                Path = CurrentDir.PathCombine(Guid.NewGuid().ToString());
+                Directory.CreateDirectory(Path);
+                FileSystem.CopyDirectory(CurrentDir.PathCombine("FilesForTesting"), Path);
+            }
+
+            public string Path { get; private set; }
+
+            public void Dispose()
+            {
+                Directory.Delete(Path, true);
+            }
+        }
     }
 }
